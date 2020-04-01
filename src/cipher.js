7d8784eb2c1329f958f38fb5f539437d67f03be7
Jucielly de Souza <juciellysouzasilva@gmail.com>
2020-04-01 13:43:39 -0300

    erro js corrigido

--- src/cipher.js
+++ src/cipher.js
@@ -1,17 +1,18 @@
 const alphabetOfsset = 65;
 const alphabetSize = 26;
 const cipher = {
-  encode: (offset,string) => {
+  encode: (offset, string) => {
     const stringToUpper = string.toUpperCase();
     let encoded = ""
-    for(let i = 0; i <= string.length; i++){
-       const letterInAsciiTable = stringToUpper.charCodeAt(i);
-       const cipherFormula = ((letterInAsciiTable - alphabetOfsset + offset)% alphabetSize)+alphabetOfsset;
-       const result = String.fromCharCode(cipherFormula)
-       encoded += result
+    for (let i = 0; i < string.length; i++) {
+      const letterInAsciiTable = stringToUpper.charCodeAt(i);
+      const cipherFormula = ((letterInAsciiTable - alphabetOfsset + offset) % alphabetSize) + alphabetOfsset;
+      const result = String.fromCharCode(cipherFormula)
+      encoded += result
     }
     return encoded
-}
+  }
+  
 };
 
 export default cipher;
