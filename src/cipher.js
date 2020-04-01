f69a51ece9d34ca792c82d20db944fce92b58c6a
Jucielly de Souza <juciellysouzasilva@gmail.com>
2020-04-01 11:53:08 -0300

    lógica encode feita

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,17 @@
+const alphabetOfsset = 65;
+const alphabetSize = 26;
 const cipher = {
-  // ...
+  encode: (offset,string) => {
+    const stringToUpper = string.toUpperCase();
+    let encoded = ""
+    for(let i = 0; i <= string.length; i++){
+       const letterInAsciiTable = stringToUpper.charCodeAt(i);
+       const cipherFormula = ((letterInAsciiTable - alphabetOfsset + offset)% alphabetSize)+alphabetOfsset;
+       const result = String.fromCharCode(cipherFormula)
+       encoded += result
+    }
+    return encoded
+}
 };
 
 export default cipher;
