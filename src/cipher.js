83d555b145d4abdb1362359577cba959517bdd6b
Jucielly de Souza <juciellysouzasilva@gmail.com>
2020-04-02 12:43:54 -0300

    encode implementado na dom

--- src/cipher.js
+++ src/cipher.js
@@ -3,10 +3,11 @@ const alphabetSize = 26;
 const cipher = {
   encode: (offset, string) => {
     const stringToUpper = string.toUpperCase();
+    const parsedOffset = parseInt(offset)
     let encoded = ""
     for (let i = 0; i < string.length; i++) {
       const letterInAsciiTable = stringToUpper.charCodeAt(i);
-      const cipherFormula = ((letterInAsciiTable - alphabetOfsset + offset) % alphabetSize) + alphabetOfsset;
+      const cipherFormula = (((letterInAsciiTable - alphabetOfsset) + parsedOffset) % alphabetSize) + alphabetOfsset;
       const result = String.fromCharCode(cipherFormula)
       encoded += result
     }
--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 import cipher from './cipher.js';
 
 console.log(cipher);
+
+//encode 
+let form = document.getElementById("form")
+//let decodeBtn = document.getElementById("decode-btn")
+let encodeBtn = document.getElementById("encode-btn")
+
+
+function encryptMessage() {
+ const messageValue = form.elements["message"].value
+ const offsetValue = form.elements["offset"].value
+ const encryptedValue = cipher.encode(offsetValue,messageValue)
+ form.elements["result"].value = encryptedValue
+}
+
+form.onsubmit = function(e){
+e.preventDefault()
+encryptMessage()
+}
+
+encodeBtn.onclick = encryptMessage
