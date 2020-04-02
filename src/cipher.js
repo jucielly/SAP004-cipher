02242f601f86a867a263a42d3ef1df8a986a3f7f
Jucielly de Souza <juciellysouzasilva@gmail.com>
2020-04-02 15:38:19 -0300

    lógica decode feita e implementada no html

--- src/cipher.js
+++ src/cipher.js
@@ -12,8 +12,20 @@ const cipher = {
       encoded += result
     }
     return encoded
+  },
+  decode: (offset, string) => {
+    const stringToUpper = string.toUpperCase();
+    const parsedOffset = parseInt(offset)
+    let decoded = ""
+    for (let i = 0; i < string.length; i++) {
+      const letterInAsciiTable = stringToUpper.charCodeAt(i);
+      const cipherFormula = (((letterInAsciiTable + alphabetOfsset) - parsedOffset) % alphabetSize) + alphabetOfsset;
+      const result = String.fromCharCode(cipherFormula)
+      decoded += result
+    }
+    return decoded
   }
-  
+
 };
 
 export default cipher;
--- src/index.js
+++ src/index.js
@@ -2,22 +2,32 @@ import cipher from './cipher.js';
 
 console.log(cipher);
 
-//encode 
+
 let form = document.getElementById("form")
-//let decodeBtn = document.getElementById("decode-btn")
+let decodeBtn = document.getElementById("decode-btn")
 let encodeBtn = document.getElementById("encode-btn")
 
 
 function encryptMessage() {
- const messageValue = form.elements["message"].value
- const offsetValue = form.elements["offset"].value
- const encryptedValue = cipher.encode(offsetValue,messageValue)
- form.elements["result"].value = encryptedValue
+  const messageValue = form.elements["message"].value
+  const offsetValue = form.elements["offset"].value
+  const encryptedValue = cipher.encode(offsetValue, messageValue)
+  form.elements["result"].value = encryptedValue
 }
 
-form.onsubmit = function(e){
-e.preventDefault()
-encryptMessage()
+form.onsubmit = function (e) {
+  e.preventDefault()
+  encryptMessage()
 }
 
 encodeBtn.onclick = encryptMessage
+
+
+function decryptMessage() {
+    const messageValue = form.elements["message"].value
+    const offsetValue = form.elements["offset"].value
+    const decryptedValue = cipher.decode(offsetValue,messageValue)
+    form.elements["result"].value = decryptedValue
+}
+
+decodeBtn.onclick = decryptMessage
