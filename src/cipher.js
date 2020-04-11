40900b1c9da68e97cc06d9e97c7a3f6bf63cc832
Jucielly de Souza <juciellysouzasilva@gmail.com>
2020-04-10 21:41:03 -0300

    (hacker edition) encode letras maiuculas e minusculas

--- src/cipher.js
+++ src/cipher.js
@@ -1,31 +1,33 @@
-const alphabetOfsset = 65;
-const alphabetSize = 26;
+//const initalUpperCaseLetters = 65
+//const endUpperCaseLetters = 90
+//const initalLowerCaseLetters = 97
+//const endLowerCaseLetters = 122
+//const alphabetSize = 26
+
 const cipher = {
+
   encode: (offset, string) => {
-    const stringToUpper = string.toUpperCase();
-    const parsedOffset = parseInt(offset)
+    if (typeof offset != "number" || typeof string != "string") {
+      throw new TypeError()
+    }
+    const parsedOffset = offset
     let encoded = ""
     for (let i = 0; i < string.length; i++) {
-      const letterInAsciiTable = stringToUpper.charCodeAt(i);
-      const cipherFormula = (((letterInAsciiTable - alphabetOfsset) + parsedOffset) % alphabetSize) + alphabetOfsset;
-      const result = String.fromCharCode(cipherFormula)
-      encoded += result
+      const letterInAsciiTable = string.charCodeAt(i);
+      if (letterInAsciiTable >= 65 && letterInAsciiTable <= 90) {
+        encoded += String.fromCharCode(((letterInAsciiTable - 65 + parsedOffset) % 26) + 65); //para upperCase
+      } else if (letterInAsciiTable >= 97 && letterInAsciiTable <= 122) {
+        encoded += String.fromCharCode(((letterInAsciiTable - 97 + parsedOffset) % 26) + 97) //para lowerCase
+      } else {
+        encoded += string.charAt(i);
+      }
+
     }
     return encoded
   },
-  decode: (offset, string) => {
-    const stringToUpper = string.toUpperCase();
-    const parsedOffset = parseInt(offset)
-    let decoded = ""
-    for (let i = 0; i < string.length; i++) {
-      const letterInAsciiTable = stringToUpper.charCodeAt(i);
-      const cipherFormula = (((letterInAsciiTable + alphabetOfsset) - parsedOffset) % alphabetSize) + alphabetOfsset;
-      const result = String.fromCharCode(cipherFormula)
-      decoded += result
-    }
-    return decoded
-  }
 
-};
+ 
 
+
+}
 export default cipher;
